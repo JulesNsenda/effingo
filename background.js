64e71101ec4aa4c820ddc09a144e0d1c90d9afc8
Jules Nsenda <jules.nsenda@gmail.com>
2023-10-20 11:40:24 +0200

    Changes to count copies accross all tabs

--- background.js
+++ background.js
@@ -1,16 +1,14 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.get({ copyCount: 0 }, (data) => {
-        let copyCount = data.copyCount;
-        chrome.browserAction.setBadgeText({ text: copyCount.toString() });
-    });
+let copyCount = 0;
 
-    chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-        if (message.type === 'copy') {
-            chrome.storage.local.get({ copyCount: 0 }, (data) => {
-                let copyCount = data.copyCount + 1;
-                chrome.storage.local.set({ copyCount });
-                chrome.browserAction.setBadgeText({ text: copyCount.toString() });
-            });
-        }
-    });
+chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
+    if (message.type === 'copy') {
+        copyCount++;
+        chrome.browserAction.setBadgeText({ text: copyCount.toString() });
+        chrome.storage.local.set({ copyCount });
+    }
+});
+
+chrome.storage.local.get({ copyCount: 0 }, (data) => {
+    copyCount = data.copyCount;
+    chrome.browserAction.setBadgeText({ text: copyCount.toString() });
 });
