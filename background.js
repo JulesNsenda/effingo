3577ce7cf12120b37863e3e3640d5f23957716eb
Jules Nsenda <jules.nsenda@gmail.com>
2023-10-20 11:59:09 +0200

    Reset the count on restart of the browser

--- background.js
+++ background.js
@@ -1,14 +1,21 @@
 let copyCount = 0;
 
-chrome.runtime.onMessage.addListener((message, _sender, _sendResponse) => {
-    if (message.type === 'copy') {
-        copyCount++;
-        chrome.browserAction.setBadgeText({ text: copyCount.toString() });
-        chrome.storage.local.set({ copyCount });
-    }
+chrome.runtime.onStartup.addListener(() => {
+    copyCount = 0;
+    chrome.storage.local.set({ copyCount: 0 });
+
+    chrome.browserAction.setBadgeText({ text: '0' });
 });
 
 chrome.storage.local.get({ copyCount: 0 }, (data) => {
     copyCount = data.copyCount;
     chrome.browserAction.setBadgeText({ text: copyCount.toString() });
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'copy') {
+        copyCount++;
+        chrome.storage.local.set({ copyCount });
+        chrome.browserAction.setBadgeText({ text: copyCount.toString() });
+    }
 });
